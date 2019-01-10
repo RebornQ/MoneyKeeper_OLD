82631240beb0c320432334fac31213e3f33ecc47
Bakumon <bakumon.me@Gmail.com>
2019-01-10 19:49:28 +0800

    test

---
+++ app/src/main/java/me/bakumon/moneykeeper/utill/DateTimeUtil.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 Bakumon. https://github.com/Bakumon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package me.bakumon.moneykeeper.utill
+
+import java.time.LocalDate
+import java.util.*
+
+/**
+ * 日期时间工具类
+ * 使用 java8 java.time API
+ *
+ * @author Bakumon https://bakumon.me
+ */
+object DateTimeUtil {
+
+    data class Result(val startDate: Date, val endDate: Date, val displayDate: String)
+
+    fun getCustomMonth(beforeOffset: Int): Result {
+
+        val customStartDay = 1
+
+        val now = LocalDate.of(2019, 1, 1)
+
+        return Result(Date(), Date(), "")
+    }
+}
--- app/src/test/java/me/bakumon/moneykeeper/ExampleUnitTest.kt
+++ app/src/test/java/me/bakumon/moneykeeper/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package me.bakumon.moneykeeper
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +14,17 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+//        assertEquals(4, 2 + 2)
+        val today = LocalDate.of(2019, 1, 31)
+        val nextWeek = today.minusMonths(-1)
+
+        val nextWeek1 = today.minusMonths(2)
+
+        System.out.println("Today is : " + today)
+        System.out.println("Date after 1 month : " + nextWeek)
+        System.out.println("Date after 2 month : " + nextWeek1)
+
+
+
     }
 }
